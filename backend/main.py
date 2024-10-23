4ecab2c4b93f7f8dae4b18014878c6171cb4f338
Max Mulder <maxmulder03@gmail.com>
2024-10-23 15:39:34 -0400

    Upgrade Packages, Resolve Dependencies, update paths

--- backend/main.py
+++ backend/main.py
@@ -15,9 +15,9 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
-DESTINATION_PATH = ""
-RESULTS_PATH = ""
-MODEL_PATH = ""
+DESTINATION_PATH = "/dnr/sample_images/"
+RESULTS_PATH = "/dnr/results"
+MODEL_PATH = "oak_wilt_demo2.h5"
 FEEDBACK_FILE_PATH = os.path.join(DESTINATION_PATH, "feedback.json")
 
 logging.basicConfig(level=logging.INFO)
